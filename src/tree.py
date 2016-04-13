72a7c5e426280820f157f29b299e54ad200ff4b3
Keith Webber <keith.e.webber@gmail.com>
2016-04-13 17:30:24 -0400

    wip

--- src/tree.py
+++ src/tree.py
@@ -16,10 +16,10 @@ class TreeInterface:
     	new_tree = Tree(root_node)
 
     	for new_node in self.nodes:
-    		# print("staring insert for id: " + new_node.id)
+    		print("staring insert for id: " + new_node.id)
     		new_tree.insert_node_under_parent(child_node=new_node)
 
-	 	return new_tree
+    	return new_tree
 
 
 class Tree:
@@ -40,67 +40,28 @@ class Tree:
 			return current_root_node
 		else:
 			for under_current in current_root_node.children:
-				print("under_current.id: "+under_current.id+" - child_node.id: " +child_node.id )
+				print("under_current.id: "+under_current.id+" - child_node.id: " +child_node.id + "desired parent_id:" + child_node.parent_id )
 				if under_current.id == child_node.parent_id:
+					print("FOUND node: " + under_current.id)
 					return under_current
 				else:
 					self.dfs_find(under_current, child_node)
 
 
-
 	def insert_node_under_parent(self, child_node=None, starting_node=None):
+		# Wrapper for DFS finding pattern
 		if starting_node == None:
 			starting_node = self.root
 
 		print("inserting_id: " + child_node.id + "parent_id => " + child_node.parent_id)
 		parent_node = self.dfs_find(starting_node, child_node)
-
+		if parent_node == None:
+			# I might consider adding better error handing here for things that are not in graph since this is 
+			# not my favorite way of handling this.
+			return None 
 		parent_node.add_child(child_node)
 
 
-
-
-
-
-
-
-
-
-# if self.id_dictionary.has_key(parent_id) is True:
-# 	# insert into that nodes children
-# 	print("existing parent: " + parent_id + " new_node.id: " + new_node.id)
-# 	# print("existing node being added: " + new_node.id)
-# 	print self.id_dictionary[parent_id]
-# 	# self.id_dictionary[parent_id].children.append(new_node)
-# 	# if new_node.id == "5":
-# 	# 	pdb.set_trace()
-# 	self.id_dictionary[parent_id].children.append(new_node)
-# 	reference_node = next(x for x in self.id_dictionary[parent_id].children if x.id == new_node.id)
-# 	self.id_dictionary[new_node.id] = reference_node
-# else:
-# 	print("NEW parent: " + parent_id + " new_node.id: " + new_node.id)
-# 	self.id_dictionary[new_node.id] = new_node
-# 	print self.id_dictionary[new_node.id].children
-
-
-# 	# for c in self.id_dictionary[parent_id].children:
-# 		# print c.parent_id
-# else: 
-# 	# print("new parent: " + parent_id)
-# 	# print("new node being added: " + new_node.id)
-# 	# insert into that node and
-# 	self.id_dictionary[new_node.id] = new_node
-# 	# for c in self.id_dictionary[parent_id].children:
-# 	# 	print c.id
-# 	# print self.id_dictionary
-
-
-# traverse tree till parent_id == new_node.parent_id
-
-
-# def 
-
-
-
-
-
+	def build_adjency_list_for_red_node_ancestor
+		# Hate this method name, but in the sake of clarity of this example
+		print('yolo')
